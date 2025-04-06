81cf74848ab5e5f53156e7f87bfd7a1f90d87215
14Sanjana <sanjanagangadevi@gmail.com>
2025-04-06 23:25:49 +0800

    Delete src/src directory

--- src/src/train.py
+++
@@ -1,8 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-import joblib
-
-def train_logistic_model(X_train, y_train):
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
-    joblib.dump(model, "../models/logistic_model.pkl")
-    return model
